8644960e4583208e90a87b343754082ba369b267
AndresPerea <magiclag12@gmail.com>
2025-04-21 08:37:52 -0500

    Added combinaciones method.

--- src/main/scala/Anagramas/package.scala
+++ src/main/scala/Anagramas/package.scala
@@ -1,11 +1,11 @@
 package object Anagramas {
     type Palabra = String
     type Frase = List[Palabra]
-    type Ocurrencias = List[(Char,Int)]
+    type Ocurrencias = List[(Char, Int)]
 
-    val diccionario: List[Palabra] = List ("cosas", "como", "yo", "y", "ocasos", "cayo", "mocosos", "roca", "moco", "sos")
+    val diccionario: List[Palabra] = List("cosas", "como", "yo", "y", "ocasos", "cayo", "mocosos", "roca", "moco", "sos")
 
-    def IOcPal(p: Palabra) : Ocurrencias ={
+    def IOcPal(p: Palabra): Ocurrencias = {
         val letras = p.toLowerCase().filter(_.isLetter)
 
         val l_group = letras.groupBy(c => c)
@@ -22,18 +22,30 @@ package object Anagramas {
         IOcPal(f_unida)
     }
 
-    lazy val diccionarioPorOcurrencias : Map[Ocurrencias, List[Palabra]] = {
+    lazy val diccionarioPorOcurrencias: Map[Ocurrencias, List[Palabra]] = {
         diccionario.groupBy(IOcPal)
     }
-/*
-    def anaagramasDePalabra(palabra: Palabra): List [Palabra] = {
 
-      // usar diccionarioPorOcurrencias,get, IOcPal
+    def anagramasDePalabra(palabra: Palabra): List[Palabra] = {
+
+        val ocurrencias = IOcPal(palabra)
+        diccionarioPorOcurrencias.getOrElse(ocurrencias, Nil)
+        // usar diccionarioPorOcurrencias,get, IOcPal
     }
 
-    def combinaciones(Iocurrencias: Ocurrencias) : List[Ocurrencias] = {
-      // usar una expresion for para producir el resultado
-    }
+    def combinaciones(Iocurrencias: Ocurrencias): List[Ocurrencias] = {
+        val opcionesPorLetra: List[List[Ocurrencias]] = Iocurrencias.map {
+            case (letra, n) =>
+                val ocurrencias = (1 to n).map(i => List((letra, i))).toList
+                Nil :: ocurrencias 
+        }
 
-*/
+        opcionesPorLetra.foldRight(List(List.empty[(Char, Int)])) {
+            (opcionesLetra, combinacionesParciales) =>
+                for {
+                    opcion <- opcionesLetra
+                    combinacion <- combinacionesParciales
+                } yield opcion ++ combinacion
+        }
+    }
 }
